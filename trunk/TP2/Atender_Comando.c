b435b742b404cbc253a160a96eafeddcadb1cf74
vegasruffmurphy@gmail.com <vegasruffmurphy@gmail.com@d0775554-b76b-a9ec-b7a6-1ce6f40b2fa8>
2012-10-05 00:44:33 +0000

    anda de 10 con vfork

--- trunk/TP2/Atender_Comando.c
+++ trunk/TP2/Atender_Comando.c
@@ -10,7 +10,16 @@
 
 
 void Atender_Comando(char* comando[5]){	
-	printf("llego");
+
+
+   pid_t pID = vfork();
+
+   if (pID == 0)                // child
+
+   {
+
+      // Code only executed by child process
+		//printf("llego");
 	char *path;
 	int i=0;
 	path=getenv("PATH");
@@ -21,22 +30,58 @@ void Atender_Comando(char* comando[5]){
 			int j;
 			for(j=0;j<BUFFSIZE;j++) temp[j]='\0';
 			strncpy(temp,path,i);
-			printf("%s\n",temp);
+			//printf("%s\n",temp);
 			strcat(temp,"/");
 			strcat(temp,comando[0]);
-			printf("%s\n",temp);
+			//printf("%s\n",temp);
 
 			if(execv(temp,comando)!=-1){
 				flag=1;
-				printf("se ejecuto\n");
+				//printf("se ejecuto\n");
 				break;
 				}
 			strcpy(path,path+i+1);
-			printf("%s\n",path);
+			//printf("%s\n",path);
 			i=0;
 			}
 		i++;
 		
 		
 		}
+		
+
+      
+     
+
+      exit(0);
+
+    }
+
+    else if (pID < 0)            // failed to fork
+
+    {
+
+        
+
+        exit(1);
+
+        // Throw exception
+
+    }
+
+    else                                   // parent
+
+    {
+		wait(0);
+      // Code only executed by parent process
+
+ 
+
+      
+
+	}	
+
+
+
+
 }
