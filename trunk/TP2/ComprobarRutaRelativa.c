f52849ae2dc450d17d508e8387d5038d501ca020
vegasruffmurphy@gmail.com <vegasruffmurphy@gmail.com@d0775554-b76b-a9ec-b7a6-1ce6f40b2fa8>
2012-10-05 02:44:08 +0000

    a

--- trunk/TP2/ComprobarRutaRelativa.c
+++ trunk/TP2/ComprobarRutaRelativa.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
-#include <string.h>
 #include <unistd.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
 
 #define BUFFSIZE 256
-
-int ComprobarRutaRelativa(char *[5]);
+#define ARGUMENTOS 10
+int ComprobarRutaRelativa(char *[ARGUMENTOS]);
 
 //int main(int argc, char *argv[])
 //{	
@@ -20,20 +24,20 @@ int ComprobarRutaRelativa(char *[5]);
 	//return 0;
 //}
 
-int ComprobarRutaRelativa(char *argv[5]){
+int ComprobarRutaRelativa(char *argv[ARGUMENTOS]){
 	
 		int largo;
 	
 		largo=strlen(argv[0]);
-		printf("Largo = %i\n",largo);
+		//printf("Largo = %i\n",largo);
 		
 		if(largo>1){
 				int i;
 				for(i=0;i<largo;i++){
 					if(argv[0][i]=='/')
-					{return 0;}
+					{return 1;}
 				}
 				
-		}		return 1;
+		}		return 0;
 }
 
