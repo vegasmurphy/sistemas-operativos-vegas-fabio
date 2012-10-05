2050443fa05f506484cb36c4bcb2d57789384a5d
vegasruffmurphy@gmail.com <vegasruffmurphy@gmail.com@d0775554-b76b-a9ec-b7a6-1ce6f40b2fa8>
2012-10-05 00:15:07 +0000

    no se

--- TP2/TP2.c
+++ TP2/TP2.c
@@ -10,7 +10,8 @@ void Atender_Comando(char* [5]);
 
 int main(int argc,char *argv[]){
 	
-	while(1)
+	printf("%s",getenv("PATH"));
+		while(1)
 		{	
 			Creador_Prompt();
 			Ingresar_Comando(argv);
@@ -46,16 +47,18 @@ void Ingresar_Comando(char *comando[5])
 	char buffer[256];
 	char *t1;
 	int i=0,h=0;
-	
+	int j=0;
 	//printf("Ingrese comando\n");
 
 	gets(buffer);
-		
+
+
 	
 	for ( t1 = strtok(buffer," "); t1 != NULL; t1 = strtok(NULL, " ") ){
 		comando[i]= t1;
 		i++;
 	}	
+	for (j=i;j<5;j++) comando[j]=NULL;
 	while(h<i){
 		//printf("%i  --------   %s\n", h, comando[h]);
 		h++;
@@ -67,10 +70,11 @@ void Ingresar_Comando(char *comando[5])
 void Verificar_Comando_Ingresado(char *comando[5])
 {
 	// Crea un proceso hijo para realizar la busqueda y ejecucion del comando ingresado
-	
+	printf("llego verificar");
 	int valor;
 	valor = Evaluar_Comando(comando);
 	//printf("valor: %i\n",valor);
+	printf("evaluar ok");
 	
 	switch (valor){
 		case 1:
@@ -89,7 +93,7 @@ void Verificar_Comando_Ingresado(char *comando[5])
 }
 
 int Evaluar_Comando(char *comando[5]){
-	
+	printf("llego evaluar");
 		// Identifica si el comando ingresado es una ruta absoluta, una ruta relativa o un comando.
 		if (!strncmp(comando[0],"cd",2)){
 			return 4;
@@ -156,24 +160,4 @@ void Atender_Ruta_Absoluta(char* comando[5]){
 	}
 }
 
-void Atender_Comando(char* comando[5]){	
-	pid_t pid_comando;
-	
-	pid_comando = fork();
-	
-	if(pid_comando == -1){
-		printf("Error");
-		fprintf(stderr, "can't fork, error %d\n", errno);
-		exit(EXIT_FAILURE);
-	}
-	if(pid_comando == 0)
-	{
-		printf("por que no entra aca?\n");
-		_exit(0);
-	}
-	else
-	{
-		printf("Padre--------Comando \n");
-		return;
-	}	
-}
+
